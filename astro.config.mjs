8ac0f491cff52cd9ce1f1fe4c309a2c5ca9dd295
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2024-02-14 17:52:07 +0000

    user registration and login (#2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
 import tailwind from '@astrojs/tailwind';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: true })],
+  output: 'hybrid',
+  integrations: [react(), tailwind({
+    applyBaseStyles: true
+  })],
+  adapter: netlify()
 });
