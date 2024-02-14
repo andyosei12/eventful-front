aaba715c0352b1ebcb9e6b64b23b5a277836ae14
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2024-02-14 17:50:23 +0000

    user registration and login

--- src/components/HomeNav.tsx
+++ src/components/HomeNav.tsx
@@ -18,7 +18,7 @@ const HomeNav = () => {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <h1>Eventful</h1>
           </a>
@@ -45,7 +45,10 @@ const HomeNav = () => {
           ))}
         </div>
         <div className="hidden lg:flex lg:items-center lg:flex-1 lg:justify-end gap-x-5 ">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a
+            href="/login"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
             Log in <span aria-hidden="true">&rarr;</span>
           </a>
           <a
@@ -92,7 +95,7 @@ const HomeNav = () => {
               </div>
               <div className="py-6">
                 <a
-                  href="#"
+                  href="/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Log in
