07281f104d225490cc6eab90fed964abe566b219
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2024-03-21 08:07:58 +0000

    ui improvement

--- src/components/PurchaseTicket.tsx
+++ src/components/PurchaseTicket.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import { useStore } from "@nanostores/react";
-import { isTicketModalOpen } from "../modalStore";
-import { ticketQRCode } from "../modalStore";
+import { useState } from 'react';
+import { useStore } from '@nanostores/react';
+import { isTicketModalOpen } from '../modalStore';
+import { ticketQRCode } from '../modalStore';
 
 type PurchaseTicketProps = {
   id?: string;
@@ -14,16 +14,16 @@ const PurchaseTicket = ({ id, price, token }: PurchaseTicketProps) => {
   const $isTicketModalOpen = useStore(isTicketModalOpen);
   const onPurchaseHandler = async () => {
     if (!token) {
-      window.location.href = "/login";
+      window.location.href = '/login';
     } else {
       setIsBooking(true);
       try {
         const ticketJson = await fetch(
           `${import.meta.env.PUBLIC_API_URL}/tickets`,
           {
-            method: "POST",
+            method: 'POST',
             headers: {
-              "Content-Type": "application/json",
+              'Content-Type': 'application/json',
               Authorization: `Bearer ${token}`,
             },
             body: JSON.stringify({
@@ -43,14 +43,15 @@ const PurchaseTicket = ({ id, price, token }: PurchaseTicketProps) => {
       }
     }
   };
+
   return (
-    <div className="fixed bottom-0 z-40 bg-[#EFFEF5] p-6 flex sm:flex-row flex-col sm:justify-between sm:items-center w-full">
+    <div className="sticky bottom-0 bg-[#EFFEF5]; z-40 p-6 flex sm:flex-row flex-col sm:justify-between sm:items-center w-full">
       <p className="text-2xl text-center mb-3 font-semibold ">GHC {price}</p>
       <button
         onClick={onPurchaseHandler}
         className="text-lg font-bold bg-[#108544] leading-6 text-white rounded-md p-4 shadow-sm"
       >
-        {isBooking ? "Processing..." : "Purchase Ticket"}
+        {isBooking ? 'Processing...' : 'Purchase Ticket'}
       </button>
     </div>
   );
