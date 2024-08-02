203c35cbb16f55bf96d584caabb5158e34302443
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2024-08-02 09:53:41 +0000

    scan ticket client side

---
+++ src/components/dashboard/TellerDashboard.tsx
@@ -0,0 +1,13 @@
+import ScanTicket from './ScanTicket';
+
+const TellerDashboard = () => {
+  return (
+    <div>
+      <div className="border rounded-lg">
+        <ScanTicket />
+      </div>
+    </div>
+  );
+};
+
+export default TellerDashboard;
