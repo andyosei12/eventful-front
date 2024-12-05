2cb2282bec7e2f8b4ed87b0bed280f7dada91c5c
Andy Osei <andrew.owusu@novanest.net>
2024-12-05 11:23:19 +0000

    enable paystack payment

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -36,34 +36,33 @@ const EventCard = ({
     if (!token) {
       window.location.href = `/login?event=${id}`;
     } else {
-      console.log('paystack is a mess')
-      // setIsBooking(true);
-      // try {
-      //   const paymentInitiationJson = await fetch(
-      //     `${import.meta.env.PUBLIC_API_URL}/payment/initiate-transaction`,
-      //     {
-      //       method: "POST",
-      //       headers: {
-      //         "Content-Type": "application/json",
-      //         Authorization: `Bearer ${token}`,
-      //       },
-      //       body: JSON.stringify({
-      //         eventId: id,
-      //       }),
-      //     }
-      //   );
-      //   const paymentInitiationData = await paymentInitiationJson.json();
-      //   console.log(paymentInitiationData);
-      //   if (paymentInitiationData.data.authorization_url) {
-      //     const paystackAuthorizationUrl =
-      //       paymentInitiationData.data.authorization_url;
-      //     window.location.href = paystackAuthorizationUrl;
-      //   }
-      // } catch (error) {
-      //   console.log(error);
-      // } finally {
-      //   setIsBooking(false);
-      // }
+      setIsBooking(true);
+      try {
+        const paymentInitiationJson = await fetch(
+          `${import.meta.env.PUBLIC_API_URL}/payment/initiate-transaction`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+              eventId: id,
+            }),
+          }
+        );
+        const paymentInitiationData = await paymentInitiationJson.json();
+        console.log(paymentInitiationData);
+        if (paymentInitiationData.data.authorization_url) {
+          const paystackAuthorizationUrl =
+            paymentInitiationData.data.authorization_url;
+          window.location.href = paystackAuthorizationUrl;
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsBooking(false);
+      }
     }
   };
   return (
